a3ad82790d8548cd1625523c9f66610d1f6bd3a4
Aram <aram_martin@alumni.brown.edu>
2021-07-15 16:10:58 -0500

    FIX: script commands && csp config in server file

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const http = require("http");
-const httpProxy = require("http-proxy");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
-const PORT = process.env.PORT || 3333;
-httpProxy.createProxyServer({ target: "http://127.0.0.1" }).listen(PORT);
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+const server = app.listen(process.env.PORT || 5500, () => {
+  const { port } = server.address();
+  console.log(`Server running on PORT ${port}`);
+});
