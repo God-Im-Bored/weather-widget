8f5a00104380d0095968ed05553ee0ce31ff4aa4
Aram <aram_martin@alumni.brown.edu>
2021-07-15 16:42:11 -0500

    FIX: cors/csp config in server

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
+
 app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy: upgrade-insecure-requests; default-src https:',
-    'Content-Security-Policy-Report-Only: default-src https:; report-uri /endpoint'
-  );
+  res.set("Content-Security-Policy", "default-src 'self'");
   next();
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 const server = app.listen(process.env.PORT || 5500, () => {
   const { port } = server.address();
-  console.log(`Server running on PORT ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
